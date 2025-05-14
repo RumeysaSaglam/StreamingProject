85d15cc13af556c8108823ef913a1f1eafe02b3a
VPN OREDATA RUMEYSA SAGLAM <rumeysa.saglam@oredata.com>
2025-05-14 11:42:23 +0300

    updated configs

--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -3,29 +3,13 @@
 set -e
 
 # Airflow versiyonunu kontrol et
-AIRFLOW_VERSION=$(airflow version | cut -d. -f1)
-
+AIRFLOW_VERSION=$(airflow version | awk -F. '{print $1}')
 echo "Detected Airflow major version: $AIRFLOW_VERSION"
 
-# Veritabanı upgrade
-echo "Running airflow db upgrade..."
+# Veritabanı migrate
+echo "Running airflow db migrate.."
 airflow db migrate
 
-
-# Sadece Airflow 2.x için kullanıcı oluştur
-# if [ "$AIRFLOW_VERSION" = "2" ]; then
-#   echo "Creating admin user..."
-#   airflow users create \
-#     --username admin \
-#     --firstname Admin \
-#     --lastname User \
-#     --role Admin \
-#     --email admin@example.com \
-#     --password admin"""
-# else
-#   echo "Skipping user creation for Airflow $AIRFLOW_VERSION"
-# fi
-
-# Webserver başlat
-echo "Starting webserver..."
-exec airflow webserver
+# Web sunucusunu başlat
+echo "Starting Airflow API server..."
+exec airflow api-server
