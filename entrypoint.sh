5da1a7e77d68d40ef426aad42f8e7ef9034bd8b1
VPN OREDATA RUMEYSA SAGLAM <rumeysa.saglam@oredata.com>
2025-05-16 01:52:38 +0300

    airflow login conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,24 @@ echo "Detected Airflow major version: $AIRFLOW_VERSION"
 
 export PYTHONPATH=$PYTHONPATH:/opt/airflow/scripts
 
-echo "Running airflow db upgrade.."
+echo "Running airflow db upgrade..."
 airflow db upgrade
+
+# Admin kullanıcıyı oluştur (eğer yoksa)
+echo "Checking for existing admin user..."
+airflow users list | grep -w "${_AIRFLOW_USERNAME}" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Creating admin user..."
+  airflow users create \
+    --username "${_AIRFLOW_USERNAME}" \
+    --firstname "${_AIRFLOW_FIRSTNAME}" \
+    --lastname "${_AIRFLOW_LASTNAME}" \
+    --role Admin \
+    --email "${_AIRFLOW_EMAIL}" \
+    --password "${_AIRFLOW_PASSWORD}"
+else
+  echo "Admin user '${_AIRFLOW_USERNAME}' already exists. Skipping creation."
+fi
 
 echo "Starting Airflow webserver..."
 exec airflow webserver
